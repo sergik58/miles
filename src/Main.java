2fea898cc56ca0ac1448d9f9404eeffc620b1b82
Сергей Поздов <torres.t2012@yandex.ru>
2022-07-06 12:54:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 1000;
+        int bonus = 20;
+        int finish = amount/bonus;
+        System.out.println(finish);
+
+    }
+}
